628aae06839c71bc6dfa70ff50c354047a741b08
Sunidhi7505 <sunidhigoyal7505@example.com>
2026-05-03 13:11:51 +0530

    fixed cors config

--- backend/src/main/java/com/foody/config/CorsConfig.java
+++ backend/src/main/java/com/foody/config/CorsConfig.java
@@ -20,7 +20,8 @@ public class CorsConfig {
                 "http://localhost:3000",
                 "http://localhost:5173",
                 "http://localhost:5500",
-                "http://127.0.0.1:5500"
+                "http://127.0.0.1:5500",
+                "https://food-store-management.vercel.app"
         ));
 
         config.setAllowedMethods(List.of(
